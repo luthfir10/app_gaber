fd9747aeeaab5cab4c75d472286b35fc12d828b5
luthfir10 <luthfirr10@gmail.com>
2022-12-11 12:54:14 +0700

    perubahan package.json

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
 import express from "express";
 import cors from "cors";
-import SittpRoutes from "./routes/SittpRoutes.js";
+import GaberRoutes from "./routes/GaberRoutes.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(SittpRoutes);
+app.use(GaberRoutes);
 
 app.listen(5000, () => console.log("Server up and Running.."));
