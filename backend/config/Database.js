115491fb1a39dfdc6a1fe3166db3b99e9f2e8336
Luthfir Rahman <45378958+luthfir10@users.noreply.github.com>
2023-08-23 13:14:33 +0700

    Update Database.js

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize";
 
-const dbtpp = new Sequelize("my_db", "admin", "10Agus95", {
-  host: "database-1.cdwry0ja3jul.us-east-2.rds.amazonaws.com",
-  port: "3306",
-  dialect: "mysql",
+const dbtpp = new Sequelize("nameDB", "username", "password", {
+  host: "url",
+  port: "port",
+  dialect: "sql",
 });
 export default dbtpp;
